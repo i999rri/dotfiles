2cf73a4f991fc13bb7e817a6a3a06b699a3d639a
i999rri <melody.hina.mk3@gmail.com>
2025-09-14 10:35:36 +0900

    add efmls

---
+++ config/nvim/lua/plugins/efmls-config-nvim.lua
@@ -0,0 +1,8 @@
+return {
+  'creativenull/efmls-configs-nvim',
+  version = 'v1.9.0',
+  config = function ()
+      require('config.efmls-config-nvim.init')
+  end,
+  dependencies = { 'neovim/nvim-lspconfig' }
+}
