86f8a3208d084186ee76d3d245e44a252f0736eb
i999rri <melody.hina.mk3@gmail.com>
2025-09-14 10:35:47 +0900

    add efm

--- config/nvim/lua/plugins/mason-lspconfig.lua
+++ config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,7 +1,7 @@
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = { "lua_ls", "ts_ls", "omnisharp", "intelephense" },
+        ensure_installed = { "lua_ls", "ts_ls", "omnisharp", "intelephense", "efm" },
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
