fccbc981e6f76d5f78d0f9e28aca3c487de6dd3d
i999rri <melody.hina.mk3@gmail.com>
2025-09-14 10:53:11 +0900

    背景の透過、行番号の装飾を追加

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,15 @@
 require("config.lazy")
 
 vim.opt.clipboard = "unnamedplus"
+
+vim.opt.number = true
+
+vim.cmd([[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+  highlight NormalNC guibg=none
+  highlight NormalSB guibg=none
+  highlight LineNr guifg=Black guibg=DarkGray ctermfg=Black ctermbg=DarkGray
+]])
