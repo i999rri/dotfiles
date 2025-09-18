0d1cbd805b1e1f62679da3cf54515c803f898a92
i999rri <melody.hina.mk3@gmail.com>
2025-09-18 16:11:41 +0900

    カラースキーマの制約により背景の透過が用意にできないため、autocmdで透過を行うための処理を実装

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -6,12 +6,27 @@ vim.opt.clipboard = "unnamedplus"
 
 vim.opt.number = true
 
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    vim.cmd([[
+      highlight Normal guibg=none ctermbg=none
+      highlight NonText guibg=none ctermbg=none
+      highlight NormalNC guibg=none ctermbg=none
+      highlight NormalSB guibg=none ctermbg=none
+      highlight SignColumn guibg=none ctermbg=none
+      highlight EndOfBuffer guibg=none ctermbg=none
+      highlight LineNr guibg=none ctermbg=none
+    ]])
+  end,
+})
+
+-- 初回適用
 vim.cmd([[
-  highlight Normal guibg=none
-  highlight NonText guibg=none
-  highlight Normal ctermbg=none
-  highlight NonText ctermbg=none
-  highlight NormalNC guibg=none
-  highlight NormalSB guibg=none
-  highlight LineNr guifg=Black guibg=DarkGray ctermfg=Black ctermbg=DarkGray
+  highlight Normal guibg=none ctermbg=none
+  highlight NonText guibg=none ctermbg=none
+  highlight NormalNC guibg=none ctermbg=none
+  highlight NormalSB guibg=none ctermbg=none
+  highlight SignColumn guibg=none ctermbg=none
+  highlight EndOfBuffer guibg=none ctermbg=none
+  highlight LineNr guibg=none ctermbg=none
 ]])
