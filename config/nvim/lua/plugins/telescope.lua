33a179eecbd3ecbbb3bf1feb20a98b59abdaa954
i999rri <melody.hina.mk3@gmail.com>
2025-09-18 14:51:30 +0900

    telescopeでファイルを検索するときに邪魔になる可能性のあるファイルを検索から除外するための設定を追加

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,19 @@
 return {
 	'nvim-telescope/telescope.nvim', 
 	tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' }
+	dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function ()
+        require('telescope').setup({
+            defaults = {
+                file_ignore_patterns = {
+                    "node_modules",
+                    ".git",
+                    "%.lock$",
+                    "dist",
+                    "build/",
+                    "%lazy-lock.json"
+                }
+            }
+        })
+    end
 }
