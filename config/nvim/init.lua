9afdabfdf6fe689e80f51fb079326b0b6abda84c
i999rri <melody.hina.mk3@gmail.com>
2025-09-18 21:34:10 +0900

    キーバインド変更

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = "\\"
+
 require("config.lazy")
 
 vim.cmd('colorscheme asiimov')
@@ -30,3 +32,19 @@ vim.cmd([[
   highlight EndOfBuffer guibg=none ctermbg=none
   highlight LineNr guibg=none ctermbg=none
 ]])
+
+vim.keymap.set('n', '<C-p>', ':Telescope file_browser<CR>', { noremap = true, silent = true })
+
+-- 誤動作を回避するために無効化
+vim.keymap.set('n', 'p', '<Nop>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<C-l>', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+
+-- Controll + j + k を押すことで Escape を同じ動きをするための設定
+vim.keymap.set('i', '<C-j><C-k>', '<Esc>', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-k><C-j>', '<Esc>', { noremap = true, silent = true })  -- 順序が逆でも対応
+
+-- 全てのマクロ記録関連キーを無効化
+vim.keymap.set('n', 'q', '<Nop>', { silent = true })
+vim.keymap.set('n', 'Q', '<Nop>', { silent = true })
+vim.keymap.set('n', '@', '<Nop>', { silent = true })  -- マクロ実行も無効化
