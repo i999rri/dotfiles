b70673b383a3d51ff3d2b9e38c86eb6ec495a596
i999rri <melody.hina.mk3@gmail.com>
2025-10-05 07:32:20 +0900

    mapleader削除、lazygitをnvim上で<Leader>kを押すことで実行できるようにした

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,3 @@
-vim.g.mapleader = "\\"
-
 require("config.lazy")
 
 vim.cmd('colorscheme asiimov')
@@ -60,3 +58,29 @@ local function show_detailed_diagnostic()
 end
 
 vim.keymap.set('n', '<C-h>', show_detailed_diagnostic, { desc = 'Show diagnostic or hover' })
+
+local Terminal  = require('toggleterm.terminal').Terminal
+
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  dir = "git_dir",
+  direction = "float",
+  float_opts = {
+    border = "rounded",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+  -- function to run on closing the terminal
+  on_close = function(term)
+    vim.cmd("startinsert!")
+  end,
+})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<Leader>k", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
