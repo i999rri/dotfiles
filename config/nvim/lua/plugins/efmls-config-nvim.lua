246e792dcb833a57a9b09461440d9da72995375c
i999rri <melody.hina.mk3@gmail.com>
2025-12-06 08:31:23 +0900

    remove: efm

--- config/nvim/lua/plugins/efmls-config-nvim.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "creativenull/efmls-configs-nvim",
-    version = "v1.9.0",
-    config = function()
-        require("config.efmls-config-nvim.init")
-    end,
-    dependencies = { "neovim/nvim-lspconfig" },
-}
--- config/nvim/lua/plugins/mason-lspconfig.lua
+++ config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,7 +1,7 @@
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = { "lua_ls", "ts_ls", "omnisharp", "intelephense", "efm" },
+        ensure_installed = { "lua_ls", "ts_ls", "omnisharp", "intelephense" },
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
